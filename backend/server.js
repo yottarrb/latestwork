9b0a243ccbc43786c350393304811febb8a8c973
unknown <yotta.rrb@gmail.com>
2024-04-04 11:08:51 +0530

    Add existing project files prior to the push to GitHub.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'https://frontend-rmkxhqdss-yottas-projects.vercel.app/',
+    origin: 'https://frontend-rmkxhqdss-yottas-projects.vercel.app',
     methods: ['GET', 'POST'], // Add other HTTP methods as needed
     optionsSuccessStatus: 200 // Some legacy browsers choke on 204
   };
